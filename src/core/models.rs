d5057adb55f0c39098933a16fbcba48222f83b02
Denis Nikolaev <niko.den.vl@gmail.com>
2025-08-21 21:46:14 +0400

    feat: create log model
    Issue: #10

---
+++ src/core/models.rs
@@ -0,0 +1,28 @@
+use chrono::NaiveDateTime;
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct LogEntry {
+    pub timestamp: Option<DateTime<FixedOffset>>,
+    pub level: Option<LogLevel>,
+    pub message: Option<String>,
+    pub logger: Option<String>,
+    pub process_id: Option<String>,
+    pub trace_id: Option<String>,
+    pub user: Option<String>,
+    pub tenant: Option<String>,
+    pub version: Option<String>,
+
+    #[serde(flatten)]
+    pub extra: HashMap<String, Value>,
+}
+
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum LogLevel {
+    Trace,
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
